92110582d007ad30a65b4e327a12e9877cd1b113
Trần Nguyễn Vinh <vinh.tn12513@sinhvien.hoasen.edu.vn>
2022-10-14 10:16:30 +0700

    express

--- app.js
+++ app.js
@@ -18,3 +18,16 @@ http.createServer(function (req, res) {
     }
 }).listen(3000);
 console.log("Listening on port 3000... ");
+const express = require("express")
+const app = express()
+
+const server = app.listen(3000, () => { // create a HTTP server on port 3000
+    console.log(`${server.address().port}`)
+});
+app.use(express.static(__dirname, { // host the whole directory
+    extensions: ["html", "htm", "gif", "png", "jpg"],
+}))
+
+app.route("/index").get((req, res) => {
+    return res.sendFile(__dirname + "/index.html")
+})
