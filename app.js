670409346b4e0d42cc0e92736161c3c7adad988e
Trần Nguyễn Vinh <vinh.tn12513@sinhvien.hoasen.edu.vn>
2022-10-14 11:38:03 +0700

    done

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-var http = require('http');
-var fs = require('fs').promises;
-var url = require('url');
-// let path = "/htmlpages";
-http.createServer(function (req, res) {
-    res.setHeader("Document", "text/html");
-    res.writeHead(200);
-    switch (req.url) {
-        case "/index":
-            fs.readFile(__dirname + "/index.html")
-                .then(contents => {
-                    res.end(contents);
-                });
-            break;
-        case "/login":
-            res.end('<h1>Login</h1>');
-            break;
-    }
-}).listen(3000);
-console.log("Listening on port 3000... ");
 const express = require("express")
 const app = express()
 
